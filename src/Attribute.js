fec77513ebac4e39db6233fe40a257c3c8f69ed0
Mickaël Allonneau <mickael.allonneau@gmail.com>
2015-12-18 13:25:58 +0100

    Attribute: added defaultValue field

--- src/Attribute.js
+++ src/Attribute.js
@@ -10,24 +10,53 @@
  */
 export default class Attribute {
 
+  static cast(val) {
+    if (Attribute.type(val) === 'String')
+      return JSON.parse(val);
+    else
+      return val;
+  }
+
   static fromXMLObject(obj) {
+    var defaultValue;
+    if (obj.$['default'] !== undefined) {
+      if (obj.$['default'].length > 0)
+        defaultValue = Attribute.cast(obj.$['default']);
+      else
+        defaultValue = '';
+    }
+    var optional;
+    if (obj.$.optional !== undefined) {
+      optional = Attribute.cast(obj.$.optional);
+    }
+    else if (obj.$.required !== undefined) {
+      optional = !Attribute.cast(obj.$.required);
+    }
+    var primaryKey = Attribute.cast(obj.$['primary-key']) || false;
+
     var attr = new Attribute(obj.$.name,
       obj.$.type,
-      JSON.parse(obj.$['primary-key'] || 'false'),
-      JSON.parse(obj.$.optional
-        || (obj.$.required === 'false' ? 'true' : 'false') // <== !required
-        || 'true'
-        )
+      primaryKey,
+      optional,
+      defaultValue
     );
 
     return attr;
   }
 
-  constructor(name, type, primaryKey = false, optional = true) {
+  static type(val) {
+    return Object.prototype.toString.call(val).match(/\[object (.*)\]/)[1];
+  }
+
+  constructor(name, type, primaryKey = false, optional = false, defaultValue = undefined) {
+    this.defaultValue = defaultValue;
     this.name = name;
-    this.optional = optional;
+    this.optional = primaryKey ? false : ((defaultValue !== undefined) || optional);
     this.primaryKey = primaryKey;
     this.required = !optional;
     this.type = type;
+
+    if (this.optional && this.defaultValue === undefined)
+      throw new Error("[jane] Attribute '" + this.name + "' is optional but no default value given");
   }
 }
