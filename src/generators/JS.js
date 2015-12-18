c9fd9cc47e9910ec8b038d9f53c79d9fc44ca492
Mickaël Allonneau <mickael.allonneau@gmail.com>
2015-12-18 15:05:40 +0100

    Added JS generator

---
+++ src/generators/JS.js
@@ -0,0 +1,90 @@
+import AbstractGenerator from './AbstractGenerator';
+
+export default class JSGenerator extends AbstractGenerator {
+
+  constructor(entity) {
+    super(entity);
+    this.indentation = 0;
+  }
+
+  indent() {
+    var str = '';
+
+    for (var i = 0; i < this.indentation * 2; i++)
+      str += ' ';
+
+    return str;
+  }
+
+  generate() {
+    var e = this.entity;
+    var str = '';
+
+    this.entity.attributes.sort(function(a, b) {
+      if (a.required && b.optional)
+        return -1;
+      else if (a.optional && b.required)
+        return 1;
+      else
+        return 0;
+    });
+
+    str += 'export default class ' + e.name + ' {\n';
+    str += '\n';
+    str += this.generateConstructor();
+    str += '\n}';
+    str += '\n';
+
+    return str;
+  }
+
+  generateConstructor() {
+    var str = '';
+
+    this.indentation++;
+    str += this.indent() + 'constructor(' + this.generateConstructorParameters() + ') {\n';
+    str += this.generateConstructorBody();
+    str += this.indent() + '}'
+    this.indentation--;
+
+    return str;
+  }
+
+  generateConstructorBody() {
+    var str = '';
+
+    this.entity.attributes.sort(function(a, b) {
+      return ~~(a.name > b.name);
+    }); // sort in alphabetical order
+
+    this.indentation++;
+    for (var attr of this.entity.attributes) {
+      str += this.indent() + 'this.' + attr.name + ' = ' + attr.name + ';\n';
+    }
+    this.indentation--;
+
+    return str;
+  }
+
+  generateConstructorParameters() {
+    var self = this;
+    var str = '';
+
+    this.entity.attributes.forEach(function(attr, i) {
+      str += attr.name;
+
+      if (attr.optional) {
+        if (attr.type !== 'String' || attr.defaultValue == null)
+          str += ' = ' + attr.defaultValue;
+        else
+          str += ' = "' + attr.defaultValue + '"';
+      }
+
+      if (i !== (self.entity.attributes.length - 1)) { // not last attribute
+        str += ', ';
+      }
+    });
+
+    return str;
+  }
+}
