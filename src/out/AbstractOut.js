7b4a0fbb18f814cfa3dfd07439db11f15f470069
Mickaël Allonneau <mickael.allonneau@gmail.com>
2015-12-18 08:47:25 +0100

    Added AbstractOut class

---
+++ src/out/AbstractOut.js
@@ -0,0 +1,20 @@
+/*
+ * Abstract output class. Should be inherited by all classes whose goal is to generate code from an Entity object.
+ */
+export default class AbstractOut {
+
+  constructor(entity) {
+    if (!entity)
+      throw '[panda] "entity" should be an instance of the Entity class'
+
+    this.entity = entity;
+  }
+
+  /*
+   * The function that Jane will call to generate code based on the Entity object passes at instanciation time.
+   * Should return a string containing the generated code.
+   */
+  generate() {
+    return null;
+  }
+}
