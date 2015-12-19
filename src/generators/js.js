d3ed46e4fbbc03c5165abae1e553a41210257a66
Mickaël Allonneau <mickael.allonneau@gmail.com>
2015-12-19 01:59:40 +0100

    JSGenerator: added some checks in constructor

--- src/generators/js.js
+++ src/generators/js.js
@@ -47,6 +47,8 @@ export default class JSGenerator extends AbstractGenerator {
     for (var attr of this.entity.attributes) {
       str += this.indent() + 'this.' + attr.name + ' = ' + attr.name + ';\n';
     }
+    str += '\n';
+    str += this.generateConstructorValidation();
     this.indentation--;
 
     return str;
@@ -70,6 +72,69 @@ export default class JSGenerator extends AbstractGenerator {
         str += ', ';
       }
     });
+
+    return str;
+  }
+
+  generateConstructorValidation() {
+    var e = this.entity;
+    var self = this;
+    var str = '';
+
+    e.attributes.forEach(function(attr, i) {
+      if (attr.required) {
+        str += self.indent() + 'if (! (' + self.generateValidationCondition('required', attr) + '))\n';
+        self.indentation++;
+        str += self.indent() + 'throw "' + self.generateValidationErrorMessage('required', attr) + '";';
+        self.indentation--;
+        str += self.indent() + '\n';
+      }
+      if (attr.maxLength !== Number.POSITIVE_INFINITY) {
+        str += self.indent() + 'if (! (' + self.generateValidationCondition('maxLength', attr) + '))\n';
+        self.indentation++;
+        str += self.indent() + 'throw "' + self.generateValidationErrorMessage('maxLength', attr) + '";';
+        self.indentation--;
+        str += self.indent() + '\n';
+      }
+    });
+
+    return str;
+  }
+
+  generateValidationCondition(type, attr) {
+    var str = '';
+
+    switch (type) {
+      case 'maxLength': {
+        str += '("" + ' + attr.name + ').length <= ' + attr.maxLength;
+      }
+      break;
+      case 'required': {
+        str += '(' + attr.name + ' !== undefined) && (' + attr.name + ' !== null)';
+      }
+      break;
+    }
+
+    return str;
+  }
+
+  generateValidationErrorMessage(type, attr) {
+    var str = '';
+
+    switch (type) {
+      case 'maxLength': {
+        str += "'" + attr.name + "' must be less than or equal to " + attr.maxLength + " characters";
+      }
+      break;
+      case 'required': {
+        str += "'" + attr.name + "' is a required field and thus it can't be null or undefined";
+      }
+      break;
+      default: {
+        throw 'Unknown error type, can\'t generate related error message for "' + attr.name + '".';
+      }
+      break;
+    }
 
     return str;
   }
