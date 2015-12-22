ed92b1badd9b366c08e6f3e5179065dc43bf2e15
Mickaël Allonneau <mickael.allonneau@gmail.com>
2015-12-22 21:01:41 +0100

    SQLiteGenerator: added foreign keys support

--- src/generators/sqlite.js
+++ src/generators/sqlite.js
@@ -27,6 +27,14 @@ export default class SQLiteGenerator extends AbstractGenerator {
 
     str += 'CREATE TABLE ' + e.plural.toLowerCase() + ' (\n';
     str += this.generateAttributes();
+
+    if (this.entity.references.length > 0) {
+      str += ',\n';
+      str += '\n';
+      str += this.generateForeignKeys();
+    }
+
+    str += '\n';
     str += ');\n';
 
     return str;
@@ -55,9 +63,35 @@ export default class SQLiteGenerator extends AbstractGenerator {
       }
 
       if (i != (e.attributes.length - 1))
-        str += ',';
+        str += ',\n';
+    });
 
-      str += '\n';
+    if ((e.attributes.length > 0) && (e.references.length > 0))
+      str += ',\n';
+
+    e.references.forEach(function(ref, i) {
+      str += self.indent() + ref.alias + ' INTEGER NOT NULL';
+
+      if (i < (e.references.length - 1))
+        str += ',\n';
+    });
+    this.indentation--;
+
+    return str;
+  }
+
+  generateForeignKeys() {
+    var e = this.entity;
+    var self = this;
+    var str = '';
+
+    this.indentation++;
+    e.references.forEach(function(ref, i) {
+      str += self.indent() + 'FOREIGN KEY (' + ref.alias + ') REFERENCES ' + ref.entity.plural + ' (' + ref.attribute + ')';
+
+      if (i < (e.references.length - 1)) {
+        str += ',\n';
+      }
     });
     this.indentation--;
 
