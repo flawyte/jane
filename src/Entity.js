a6a4538981ebc560cbe96a760f4664641b8fcd6a
Mickaël Allonneau <mickael.allonneau@gmail.com>
2015-12-22 15:48:07 +0100

    Entity: +|instances array +|add(entity) +|get(name)

--- src/Entity.js
+++ src/Entity.js
@@ -11,6 +11,14 @@ import Reference from './Reference';
  */
 export default class Entity {
 
+  static add(entity) {
+    if (Entity.instances === undefined)
+      Entity.instances = {};
+
+    Entity.instances[entity.name] = entity;
+    console.log(Entity.instances);
+  }
+
   static fromXMLObject(obj) {
     var ent = new Entity(obj.$.name, obj.$.plural);
 
@@ -32,11 +40,24 @@ export default class Entity {
     return ent;
   }
 
+  static get(name) {
+    if (!Entity.instances)
+      return null;
+    if (!Entity.instances[name]) {
+      console.log('Unknown entity with name "' + name + '"');
+      return null;
+    }
+
+    return Entity.instances[name];
+  }
+
   constructor(name, plural = null) {
     this.attributes = [];
     this.name = name;
     this.plural = plural;
     this.references = [];
+
+    Entity.add(this);
   }
 
   addAttribute(attr) {
@@ -44,6 +65,7 @@ export default class Entity {
   }
 
   addReference(ref) {
+    ref.entity = Entity.get(ref.entity);
     ref.source = this;
     this.references.push(ref);
   }
