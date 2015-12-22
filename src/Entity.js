7f7a8c14d97b7afa5e1ef2be676c45b6e75450a6
Mickaël Allonneau <mickael.allonneau@gmail.com>
2015-12-22 18:47:58 +0100

    Revert "Entity: +|instances array +|add(entity) +|get(name)"
    This reverts commit a6a4538981ebc560cbe96a760f4664641b8fcd6a.

--- src/Entity.js
+++ src/Entity.js
@@ -11,14 +11,6 @@ import Reference from './Reference';
  */
 export default class Entity {
 
-  static add(entity) {
-    if (Entity.instances === undefined)
-      Entity.instances = {};
-
-    Entity.instances[entity.name] = entity;
-    console.log(Entity.instances);
-  }
-
   static fromXMLObject(obj) {
     var ent = new Entity(obj.$.name, obj.$.plural);
 
@@ -40,24 +32,11 @@ export default class Entity {
     return ent;
   }
 
-  static get(name) {
-    if (!Entity.instances)
-      return null;
-    if (!Entity.instances[name]) {
-      console.log('Unknown entity with name "' + name + '"');
-      return null;
-    }
-
-    return Entity.instances[name];
-  }
-
   constructor(name, plural = null) {
     this.attributes = [];
     this.name = name;
     this.plural = plural;
     this.references = [];
-
-    Entity.add(this);
   }
 
   addAttribute(attr) {
@@ -65,7 +44,6 @@ export default class Entity {
   }
 
   addReference(ref) {
-    ref.entity = Entity.get(ref.entity);
     ref.source = this;
     this.references.push(ref);
   }
